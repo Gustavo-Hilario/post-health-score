a1bc2680da309dbdeef9a7f966ed261c3288976e
Gustavo_H <gustavo.hilario@automattic.com>
2025-12-30 20:17:33 -0500

    Add minimal plugin structure
    - Create main plugin file with WordPress plugin header
    - Include security check to prevent direct access
    - Plugin can now be activated in WordPress admin
    
    No functionality yet - just the foundation.

---
+++ post-health-score.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Post Health Score
+ * Plugin URI: https://github.com/Gustavo-Hilario/post-health-score
+ * Description: Adds a content quality health score column to the Posts admin list.
+ * Version: 1.0.0
+ * Author: Gustavo Hilario
+ * Author URI: https://github.com/Gustavo-Hilario
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: post-health-score
+ */
+
+// Prevent direct access
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+// Plugin code will be added in subsequent commits
